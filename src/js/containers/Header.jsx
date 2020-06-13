42eb384fe53e40c9d8617c112e671873b03cce05
herkar95 <herman.karlsson@hotmail.se>
2020-06-13 22:23:13 +0200

    small changes to heading

--- src/js/containers/Header.jsx
+++ src/js/containers/Header.jsx
@@ -5,37 +5,38 @@ class Header extends Component {
   render() {
     return (
       <>
-        <div id="header" className="text-center mt-4">
-          <div className="row justify-content-center">
-            <span className="col-12">
+        <div id='header' className='text-center mt-4'>
+          <div className='row justify-content-center'>
+            <span className='col-12'>
               <img
                 src={require('../../images/icons/header-logo.png')}
-                alt=""
-                className="logo"
+                alt='Logo'
+                title='Logo'
+                className='logo'
               />
             </span>
-            <h1 className="align-self-center mt-4">OMDb API Call</h1>
+            <h1 className='align-self-center mt-4 display-4'>OMDb API Call</h1>
           </div>
           <form
-            className="d-flex mt-3 col-12 col-lg-10 justify-content-center mx-auto p-0"
+            className='d-flex mt-3 col-12 col-lg-10 justify-content-center mx-auto p-0'
             onSubmit={(e) => {
               e.preventDefault();
               this.props.onSearchRequest();
             }}
           >
-            <div className="col-12 row justify-content-center p-0">
+            <div className='col-12 row justify-content-center p-0'>
               <input
-                type="text"
-                name="title"
-                placeholder="Search for a movie, series or game"
-                className="col-12 col-md-7 form-control mr-md-2 "
+                type='text'
+                name='title'
+                placeholder='Search for a movie, series or game'
+                className='col-12 col-md-7 form-control mr-md-2 '
                 autoFocus
                 onChange={(e) => this.props.onUpdateQuery(e.target.value)}
               />
               <input
-                type="submit"
-                value="Search"
-                className="btn mt-2 mt-md-0 col-12 col-md-4"
+                type='submit'
+                value='Search'
+                className='btn mt-2 mt-md-0 col-12 col-md-4'
               />
             </div>
           </form>
