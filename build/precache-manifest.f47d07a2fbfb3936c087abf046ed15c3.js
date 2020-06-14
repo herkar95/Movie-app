f5db1f5eb84e3b2cedd20e4134b85df343036305
herkar95 <herman.karlsson@hotmail.se>
2020-06-14 23:35:02 +0200

    added production build for github pages

---
+++ build/precache-manifest.f47d07a2fbfb3936c087abf046ed15c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909d178b26eb671e1ddd03d757808c58",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "d204a34dfd452d689150",
+    "url": "/Movie-app/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "85839fd135fc5ebf1e5c",
+    "url": "/Movie-app/static/css/main.b9ed2fc4.chunk.css"
+  },
+  {
+    "revision": "d204a34dfd452d689150",
+    "url": "/Movie-app/static/js/2.c5320c16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-app/static/js/2.c5320c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85839fd135fc5ebf1e5c",
+    "url": "/Movie-app/static/js/main.5a36162e.chunk.js"
+  },
+  {
+    "revision": "f4a6150a26a4887f9371",
+    "url": "/Movie-app/static/js/runtime-main.2df4f002.js"
+  }
+]);
