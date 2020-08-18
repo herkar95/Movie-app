7efb722e38dc5588f2f6bad52262ac8b7b22839c
herkar95 <herman.karlsson@hotmail.se>
2020-08-18 20:36:57 +0200

    Updates

---
+++ precache-manifest.bc2f72eb9ff4d9f82e1bcd5f3e9c816a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e06c14221728ada5b5a42c1d915db096",
+    "url": "/Movie-app/index.html"
+  },
+  {
+    "revision": "de73843cdc80f8408c80",
+    "url": "/Movie-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f8fd8a3637208f51bfa9",
+    "url": "/Movie-app/static/css/main.b9ed2fc4.chunk.css"
+  },
+  {
+    "revision": "de73843cdc80f8408c80",
+    "url": "/Movie-app/static/js/2.2549f681.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-app/static/js/2.2549f681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8fd8a3637208f51bfa9",
+    "url": "/Movie-app/static/js/main.d3e204c1.chunk.js"
+  },
+  {
+    "revision": "7e11988b311f6639fa84",
+    "url": "/Movie-app/static/js/runtime-main.7386978d.js"
+  }
+]);
